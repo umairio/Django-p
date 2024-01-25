605b0655c3e7c4c0a355721342b2412079ff5d11
umairio <umairmateen55@gmail.com>
2024-01-26 02:48:17 +0500

    made filter search and actions

--- apprac/admin.py
+++ apprac/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Customer, Product, Order
+from django.db import models
 
-# Register your models here.
+def increase_price_by_10000(modeladmin, request, queryset):
+    queryset.update(price=models.F('price') + 10000)
+
+def decrease_price_by_10000(modeladmin, request, queryset):
+    queryset.update(price=models.F('price') - 10000)
+
+def order_completed(modeladmin, request, queryset):
+    queryset.update(status='completed')
+
+def order_pending(modeladmin, request, queryset):
+    queryset.update(status='pending')
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["name", "phone_number"]
+    list_filter = ["phone_number"]
+    search_fields = ["name",'phone_number']
+    ordering = ['name']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name",'company', "price"]
+    list_filter = ["company",'price']
+    search_fields = ["name",'company','price']
+    ordering = ['name','price']
+    actions = [increase_price_by_10000,decrease_price_by_10000]
+    
+    
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'get_products', 'status']
+    list_filter = ['id', 'customer', 'status']
+    search_fields = ['id', 'customer', 'status']
+    ordering = ['status']
+    actions = [order_completed,order_pending]
+
+
+    def get_products(self, obj):
+        return ", ".join([str(product) for product in obj.products.all()])
+
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Order,OrderAdmin)
--- apprac/models.py
+++ apprac/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib import admin
+
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=15)
+
     def __str__(self):
-        return self.title
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=20)
+    company = models.CharField(max_length=20)
+    price = models.IntegerField()
+
     def __str__(self):
-        return self.title
+        return self.name
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=10)
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('shipped', 'Shipped'),
+    ]
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
 
     def __str__(self):
-        return self.title
+        return f"Order {self.id} - {self.customer} - {self.status}"
