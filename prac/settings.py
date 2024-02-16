2435d86f8a137a799a2f76d39081626cc31ef1b3
umairio <umairmateen55@gmail.com>
2024-02-17 04:34:58 +0500

    added simple middleware

--- prac/settings.py
+++ prac/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apprac.middlware.mw.testmw'
 ]
 
 ROOT_URLCONF = 'prac.urls'
